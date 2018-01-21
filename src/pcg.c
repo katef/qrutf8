f456b267622fc5ba0d728dbaa34b03ce2483f9de
Kate F <kate@elide.org>
2018-01-21 15:35:49 +0100

    Cruft.

--- src/pcg.c
+++ src/pcg.c
@@ -32,15 +32,6 @@
 
 #include "pcg.h"
 
-/* state for global RNGs */
-static pcg32_random_t pcg32_global = PCG32_INITIALIZER;
-
-/*
- * pcg32_srandom(initstate, initseq)
- * pcg32_srandom_r(rng, initstate, initseq):
- *     Seed the rng.  Specified in two parts, state initializer and a
- *     sequence selection constant (a.k.a. stream id)
- */
 void
 pcg32_srandom_r(pcg32_random_t* rng, uint64_t initstate, uint64_t initseq)
 {
@@ -51,17 +42,6 @@ pcg32_srandom_r(pcg32_random_t* rng, uint64_t initstate, uint64_t initseq)
 	pcg32_random_r(rng);
 }
 
-void
-pcg32_srandom(uint64_t seed, uint64_t seq)
-{
-	pcg32_srandom_r(&pcg32_global, seed, seq);
-}
-
-/*
- * pcg32_random()
- * pcg32_random_r(rng)
- *     Generate a uniformly distributed 32-bit random number
- */
 uint32_t
 pcg32_random_r(pcg32_random_t* rng)
 {
@@ -72,17 +52,6 @@ pcg32_random_r(pcg32_random_t* rng)
 	return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
 }
 
-uint32_t
-pcg32_random()
-{
-	return pcg32_random_r(&pcg32_global);
-}
-
-/*
- * pcg32_boundedrand(bound):
- * pcg32_boundedrand_r(rng, bound):
- *     Generate a uniformly distributed number, r, where 0 <= r < bound
- */
 uint32_t
 pcg32_boundedrand_r(pcg32_random_t* rng, uint32_t bound)
 {
@@ -118,11 +87,5 @@ pcg32_boundedrand_r(pcg32_random_t* rng, uint32_t bound)
 			return r % bound;
 		}
 	}
-}
-
-uint32_t
-pcg32_boundedrand(uint32_t bound)
-{
-	return pcg32_boundedrand_r(&pcg32_global, bound);
 }
 
