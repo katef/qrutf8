f2536ec3dac0b972b08b0ed57c58dace0237da1e
Kate F <kate@elide.org>
2018-01-29 22:29:56 +0100

    Separate assertions.

--- src/datastream.c
+++ src/datastream.c
@@ -80,7 +80,8 @@ append_bit(bool v, void *buf, size_t *bits)
 void
 append_bits(uint32_t v, size_t n, void *buf, size_t *count)
 {
-	assert(n <= CHAR_BIT * sizeof v && v >> n == 0);
+	assert(n <= CHAR_BIT * sizeof v);
+	assert(v >> n == 0);
 
 	for (int i = n - 1; i >= 0; i--) {
 		append_bit((v >> i) & 1, buf, count);
